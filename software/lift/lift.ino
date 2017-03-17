e5ea20700fc21a7d983569aabe774e1b7d49d1d4
J Kim <jennifer.ne.kim@gmail.com>
2017-03-16 20:08:58 -0400

    lift code, complete edition

--- software/lift/lift.ino
+++ software/lift/lift.ino
@@ -41,23 +41,25 @@ void setup() {
 
   delay(1200);
 
-  //once at the top, start winch
   RightMotor.VexMotorWrite(0);
   LeftMotor.VexMotorWrite(0);
 
   delay(1000);
-  
-  LiftMotor.VexMotorWrite(-10);
+
+    //once at the top, start winch
+
+  LiftMotor.VexMotorWrite(10);
   RightMotor.VexMotorWrite(50);
   LeftMotor.VexMotorWrite(50);
-  WinchMotor.VexMotorWrite(50);
+  //WinchMotor.VexMotorWrite(100); // +ve
 
   delay(350);
   RightMotor.VexMotorWrite(0);
   LeftMotor.VexMotorWrite(0);
   LiftMotor.VexMotorWrite(0);
 
-  delay(2500);
+// 7.05 seconds to get to the wall
+   delay(7000);
 
   //at the bottom, stop winch
   WinchMotor.VexMotorWrite(0);
